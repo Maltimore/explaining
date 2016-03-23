a8f727cd614a646fb735da36ab066fc6aedc47b2
Maltimore <malte.esders@gmail.com>
2016-03-23 12:49:47 +0100

    general relevance algorithm somewhat working, extended to arbitrary amount of layers and activation function

--- test1.py
+++ test1.py
@@ -3,48 +3,71 @@ import numpy as np
 import theano
 import theano.tensor as T
 import lasagne
+import matplotlib.pyplot as plt
 
 
 # parameters
 model = 'mlp'
-num_epochs = 100
+num_epochs = 50
 minibatch_size = 20
-noise_scale = .3
+noise_scale = .1
+loss_choice = "MSE"
 
 # some global variables
 INPUT_DIM = 10
 
+# controlling the behavior
+do_plotting = True
 
-def load_dataset(noise_scale=.6):
-    def create_N_examples(N=500, noise_scale=.6):
-        pic = np.zeros((10, 10))
-        pic[2:8, 2] = 1
-        pic[2:8, 7] = 1
-        pic[2, 2:8] = 1
-        pic[7, 2:8] = 1
-        overall_idx = 0
-        X = np.empty((N, 1, 10, 10))
+
+def get_target(target, loss_choice):
+    if loss_choice == "categorical_crossentropy":
+        return target
+    elif loss_choice == "MSE":
+        target_vec = np.ones(4) * (-1)
+        target_vec[target] = 1
+        return target_vec
+
+def get_category(target, loss_choice):
+    if loss_choice == "categorical_crossentropy":
+        return target
+    elif loss_choice == "MSE":
+        return np.argmax(target)
+
+def create_N_examples(loss_choice, N=500, noise_scale=.6):
+    pic = np.zeros((10, 10))
+    pic[2:8, 2] = 1
+    pic[2:8, 7] = 1
+    pic[2, 2:8] = 1
+    pic[7, 2:8] = 1
+    overall_idx = 0
+    X = np.empty((N, 1, 10, 10))
+    if loss_choice == "categorical_crossentropy":
         y = np.empty(N, dtype=np.int32)
-        while overall_idx < N:
-            for target in np.arange(4):
-                current_pic = pic.copy()
-                if target == 0:
-                    current_pic[2:8, 2] = 0
-                elif target == 1:
-                    current_pic[2:8, 7] = 0
-                elif target == 2:
-                    current_pic[2, 2:8] = 0
-                elif target == 3:
-                    current_pic[7, 2:8] = 0
-                current_pic += np.random.normal(size=(INPUT_DIM, INPUT_DIM))*noise_scale
-                X[overall_idx, 0, :, :] = current_pic
-                y[overall_idx] = target
-                overall_idx += 1
-        return X, y
+    elif loss_choice == "MSE":
+        y = np.empty((N, 4), dtype=np.int32)
+    while overall_idx < N:
+        for target in np.arange(4):
+            current_pic = pic.copy()
+            if target == 0:
+                current_pic[2:8, 2] = 0
+            elif target == 1:
+                current_pic[2:8, 7] = 0
+            elif target == 2:
+                current_pic[2, 2:8] = 0
+            elif target == 3:
+                current_pic[7, 2:8] = 0
+            current_pic += np.random.normal(size=(INPUT_DIM, INPUT_DIM))*noise_scale
+            X[overall_idx, 0, :, :] = current_pic
+            y[overall_idx] = get_target(target, loss_choice)
+            overall_idx += 1
+    return X, y
 
-    X_train, y_train = create_N_examples(500, noise_scale)
-    X_val, y_val = create_N_examples(100, noise_scale)
-    X_test, y_test = create_N_examples(100, noise_scale)
+
+def load_dataset(loss_choice, noise_scale=.6):
+    X_train, y_train = create_N_examples(loss_choice, 500, noise_scale)
+    X_val, y_val = create_N_examples(loss_choice, 100, noise_scale)
+    X_test, y_test = create_N_examples(loss_choice, 100, noise_scale)
     return X_train, y_train, X_val, y_val, X_test, y_test
 
 
@@ -69,7 +92,7 @@ def build_mlp(input_var=None):
     # Finally, we'll add the fully-connected output layer, of 10 softmax units:
     l_out = lasagne.layers.DenseLayer(
             l_hid1, num_units=4,
-            nonlinearity=lasagne.nonlinearities.softmax)
+            nonlinearity=lasagne.nonlinearities.tanh)
 
     # Each layer is linked to its incoming layer(s), so we only need to pass
     # the output layer to give access to a network in Lasagne:
@@ -91,11 +114,15 @@ def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
 
 # Load the dataset
 print("Loading data...")
-X_train, y_train, X_val, y_val, X_test, y_test = load_dataset(noise_scale)
+X_train, y_train, X_val, y_val, X_test, y_test = load_dataset(loss_choice,
+                                                              noise_scale)
 
 # Prepare Theano variables for inputs and targets
 input_var = T.tensor4('inputs')
-target_var = T.ivector('targets')
+if loss_choice == "categorical_crossentropy":
+    target_var = T.ivector('targets')
+elif loss_choice == "MSE":
+    target_var = T.dmatrix('targets')
 
 # Create neural network model (depending on first command line parameter)
 print("Building model and compiling functions...")
@@ -106,8 +133,23 @@ all_layers = lasagne.layers.get_all_layers(network)
 # Create a loss expression for training, i.e., a scalar objective we want
 # to minimize (for our multi-class problem, it is the cross-entropy loss):
 prediction = lasagne.layers.get_output(network)
-loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
-loss = loss.mean()
+
+#output = theano.function([input_var], prediction)
+#print(output(X_train[:1]))
+#np.shape(X_train[:1])
+
+
+if loss_choice == "categorical_crossentropy":
+    # loss option 1: categorical crossentropy
+    loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
+    loss = loss.mean()
+elif loss_choice == "MSE":
+    # loss option 2: MSE with {-1,1} targets
+    loss = lasagne.objectives.squared_error(prediction, target_var)
+    loss = loss.mean()
+    ## PROBABLY HAVE TO ADD loss = loss.mean() HERE!
+
+
 # We could add some weight decay as well here, see lasagne.regularization.
 
 # Create update expressions for training, i.e., how to modify the
@@ -124,9 +166,20 @@ test_prediction = lasagne.layers.get_output(network, deterministic=True)
 test_loss = lasagne.objectives.categorical_crossentropy(test_prediction,
                                                         target_var)
 test_loss = test_loss.mean()
+
+if loss_choice == "MSE":
+    test_loss = lasagne.objectives.squared_error(test_prediction,
+                                                 target_var)
+    test_loss = test_loss.mean()
 # As a bonus, also create an expression for the classification accuracy:
 test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), target_var),
                     dtype=theano.config.floatX)
+if loss_choice == "MSE":
+    test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), T.argmax(target_var, axis=1)),
+                        dtype=theano.config.floatX)
+
+
+
 
 # Compile a function performing a training step on a mini-batch (by giving
 # the updates dictionary) and returning the corresponding training loss:
@@ -190,54 +243,80 @@ def softmax(x):
     return np.exp(x) / np.sum(np.exp(x))
 
 
+def get_target_title(target):
+    if target == 0:
+        target_title = "left open"
+    elif target == 1:
+        target_title = "right open"
+    elif target == 2:
+        target_title = "top open"
+    elif target == 3:
+        target_title = "bottom open"
+    elif target == "ones":
+        target_title = "just ones"
+    return target_title
 
-def compute_relevance(X, network, target, plot_heatmap=True):
-    def plot_heatmaps(X, R_i, target):
-        if target == 0:
-            target_title = "left open"
-        elif target == 1:
-            target_title = "right open"
-        elif target == 2:
-            target_title = "top open"
-        elif target == 3:
-            target_title = "bottom open"
-        import matplotlib.pyplot as plt
-        fig, axes = plt.subplots(1, 2, figsize=(15, 10))
-        plot1 = axes[0].pcolor(X)
-        plt.colorbar(plot1, ax=axes[0])
-        axes[0].set_title("Input image")
-        plot2 = axes[1].pcolor(R_i)
-        axes[1].set_title("Relevance, " + target_title)
-        plt.colorbar(plot1, ax=axes[1])
-        plt.show()
 
-    W1_var, b1_var, W2_var, b2_var = lasagne.layers.get_all_params(network)
-    W1 = W1_var.get_value().T
-    W2 = W2_var.get_value().T
-    b1 = b1_var.get_value().T
-    b2 = b2_var.get_value().T
-    X_flat = X.flatten()
-    hid1_z = np.dot(W1, X_flat) + b1
-    hid1_act = np.copy(hid1_z)
-    hid1_act[hid1_act < 0] = 0
-    output_layer_z = np.dot(W2, hid1_act.flatten()) + b2
-    output_layer_act = softmax(output_layer_z)
-    output_layer_act
+def plot_heatmaps(X, target, R_i, output_neuron):
+    input_target_title = get_target_title(target)
+    relevance_target_title = get_target_title(output_neuron)
 
-    W_z_mat1 = np.multiply(W1, X_flat.T)
-    #W_z_mat2 = np.multiply(W2, hid1_act.T)
+    fig, axes = plt.subplots(1, 2, figsize=(15, 10))
+    plot1 = axes[0].pcolor(X)
+    plt.colorbar(plot1, ax=axes[0])
+    axes[0].invert_yaxis()
+    axes[0].set_title("Input image with " + input_target_title)
+    plot2 = axes[1].pcolor(R_i)
+    axes[1].set_title("Relevance for output neuron " + relevance_target_title)
+    axes[1].invert_yaxis()
+    plt.colorbar(plot2, ax=axes[1])
 
-    R_k_over_z_k = output_layer_act[target] / output_layer_z[target]
-    R_j = np.multiply(W2[0,:].T, hid1_act) * R_k_over_z_k
 
-    R_j_over_z_j = np.divide(R_j, hid1_z)
-    R_i = np.sum(np.multiply(W_z_mat1.T, R_j_over_z_j), axis=1)
-    R_i = R_i.reshape((10, 10))
+def compute_relevance(Input, target, network, output_neuron,  plot_heatmap=False,
+                      epsilon = .001):
+    # --- get paramters and activations for the input ---
+    all_params = lasagne.layers.get_all_params(network)
+    W_mats = all_params[0::2]
+    biases = all_params[1::2]
+    get_activations = theano.function([input_var],
+                lasagne.layers.get_output(lasagne.layers.get_all_layers(network)))
+    activations = get_activations(np.expand_dims(np.expand_dims(Input, axis=0), axis=0))
+    # loop over W_mats, biases and activations to extract values and
+    # flatten/transpose
+    idx = 0
+    for W, b, X in zip(W_mats, biases, activations):
+        W_mats[idx] = W.get_value().T
+        biases[idx] = b.get_value()
+        activations[idx] = X[0].flatten()
+        idx += 1
+    # since there is one more layer than weight matrices/biases, extract last
+    # layers values
+    activations[idx] = activations[idx][0].flatten()
+
+
+    # --- forward propagation to compute preactivations ---
+    preactivations = []
+    for W, b, X in zip(W_mats, biases, activations):
+        preactivation = np.dot(W, X) + b
+        preactivations.append(preactivation)
+
+    # --- relevance backpropagation ---
+    # the first backpass is special so it can't be in the loop
+    R_over_z = activations[-1][output_neuron] / (preactivations[-1][output_neuron] + epsilon)
+    R = np.multiply(W_mats[-1][output_neuron,:].T, activations[-2]) * R_over_z
+    for idx in np.arange(2, len(activations)):
+        R_over_z = np.divide(R, preactivations[-idx] + epsilon)
+        Z_ij = np.multiply(W_mats[-idx], activations[-idx-1] + epsilon)
+        R = np.sum(np.multiply(Z_ij.T, R_over_z), axis=1)
+    R = R.reshape((10, 10))
+
     if plot_heatmap:
-        plot_heatmaps(X, R_i, target)
-    return R_i
+        plot_heatmaps(Input, get_category(target, loss_choice), R, output_neuron)
+    return R
 
-
-target = 3
-R_i = compute_relevance(X_train[0][0], network, target)
-R_i = compute_relevance(np.ones((10,10)), network, target)
+output_neuron = 2
+dataset = 402
+R = compute_relevance(X_train[dataset][0], y_train[dataset], network, output_neuron, plot_heatmap=do_plotting)
+R = compute_relevance(np.ones((10,10)), "ones", network, output_neuron, plot_heatmap=do_plotting)
+if do_plotting:
+    plt.show()
