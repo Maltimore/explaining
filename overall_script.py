88c218a648e2bde58088ef7bb2bd3414c3b44df5
Maltimore <malte.esders@gmail.com>
2017-04-17 19:12:08 +0200

    started switch to using categorical crossentropy everywhere (not binary crossentropy)

--- overall_script.py
+++ overall_script.py
@@ -258,15 +258,16 @@ def build_cnn(params, input_var):
 
 
 def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
+    """
+    this function makes no assumption about the shape of inputs and 
+    targets. It just assumes that they have the same length.
+    """
     assert len(inputs) == len(targets)
+    indices = np.arange(len(inputs))
     if shuffle:
-        indices = np.arange(len(inputs))
         np.random.shuffle(indices)
     for start_idx in range(0, len(inputs) - batchsize + 1, batchsize):
-        if shuffle:
-            excerpt = indices[start_idx:start_idx + batchsize]
-        else:
-            excerpt = slice(start_idx, start_idx + batchsize)
+        excerpt = indices[start_idx:start_idx + batchsize]
         yield inputs[excerpt], targets[excerpt]
 
 
